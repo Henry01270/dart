dd5b38428514334fd3cf188723f16bd967e028f1
Henry01270 <118987076+Henry01270@users.noreply.github.com>
2025-03-16 13:49:23 +0000

    25_0316_1

---
+++ Dart_#3_functional/2.mapping.dart
@@ -0,0 +1,26 @@
+void main() {
+    Map<String, String> harryPotter = {
+        'Harry Potter' : '해리 포터',
+        'Ron Weasley' : '론 위즐리',
+        'Hermione Granger' : '헤르미온느 그레인저',
+    };
+    print(harryPotter);
+
+    print('----Mapping----');
+    final result = harryPotter.map(
+        (key, value) => MapEntry(
+            'Harry Potter Character  $key',
+            '해리포터 캐릭터 $value',
+        ),
+    );
+
+    print(result);
+
+    print('----Keys & Values Mapping----');
+    final keys = harryPotter.keys.map((x) => 'HPC $x').toList();
+    final values = harryPotter.values.map((x) => '해리포터 $x').toList();
+    print(keys);
+    print(values);
+
+    
+}
---
+++ Dart_#3_functional/5.reduce.dart
@@ -0,0 +1,13 @@
+void main() {
+    List<int numbers = [
+        1,
+        3,
+        5,
+        7,
+        9,
+    ];
+
+    numbers.reduce([prev, next]{
+
+    });
+}
